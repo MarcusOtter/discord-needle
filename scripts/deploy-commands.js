93aa427d9737dd3616bb9bec30e4d5ca4b250d24
Marcus Otterström <35617441+MarcusOtter@users.noreply.github.com>
2021-09-17 16:18:43 +0200

    Ignore deploy-command if no dev config is present

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // IMPORTANT: You need to `tsc` before running this script.
 
+// TODO: Probably make this a separate script (don't try to run it on start)
+
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 
@@ -8,6 +10,11 @@ const { getAllCommands } = require("../dist/handlers/commandHandler");
 const { getConfig } = require("../dist/helpers/configHelpers");
 
 const CONFIG = getConfig();
+if (!CONFIG) { return; }
+if (!CONFIG.dev) { return; }
+if (!CONFIG.dev.clientId || CONFIG.dev.clientId === "") { return; }
+if (!CONFIG.dev.guildId || CONFIG.dev.guildId === "") { return; }
+
 const rest = new REST({ version: "9" }).setToken(CONFIG.discordApiToken);
 
 (async () => {
