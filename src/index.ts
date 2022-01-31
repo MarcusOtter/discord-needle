32b90b72725f910d4d77e76412abcccb5e3c24a8
Nicholas Christopher <nchristopher@tuta.io>
2022-01-31 06:38:56 -0800

    ✨ Purge configuration data of removed guilds (#39)
    * ✨ Delete configs of removed guilds
    
    * Move deletion to configHelpers
    
    * Move logging to reset function
    
    Co-authored-by: MarcusOtter <github@otterstrom.dev>

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Client, Intents } from "discord.js";
 import { getOrLoadAllCommands } from "./handlers/commandHandler";
 import { handleInteractionCreate } from "./handlers/interactionHandler";
 import { handleMessageCreate } from "./handlers/messageHandler";
-import { getApiToken } from "./helpers/configHelpers";
+import { deleteConfigsFromUnkownServers, getApiToken, resetConfigToDefault } from "./helpers/configHelpers";
 
 (async () => {
 	(await import("dotenv")).config();
@@ -11,10 +11,14 @@ import { getApiToken } from "./helpers/configHelpers";
 	await getOrLoadAllCommands(false);
 
 	const CLIENT = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-	CLIENT.once("ready", () => console.log("Ready!"));
+	CLIENT.once("ready", () => {
+		console.log("Ready!");
+		deleteConfigsFromUnkownServers(CLIENT);
+	});
 
 	CLIENT.on("interactionCreate", interaction => handleInteractionCreate(interaction).catch(e => console.log(e)));
 	CLIENT.on("messageCreate", message => handleMessageCreate(message).catch(e => console.log(e)));
+	CLIENT.on("guildDelete", guild => { resetConfigToDefault(guild.id); });
 
 	CLIENT.login(getApiToken() ?? undefined);
 })();
