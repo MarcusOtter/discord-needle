78fe3d946cbad356b5ae4b0f2ee9d0c6b270a448
c43721 <55610086+c43721@users.noreply.github.com>
2022-04-16 15:06:32 -0500

    fix: fix dapi imports (#134)

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -17,7 +17,7 @@
 
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { type CommandInteraction, MessageActionRow, MessageEmbed } from "discord.js";
-import type { APIApplicationCommandOption } from "discord-api-types";
+import type { APIApplicationCommandOption } from "discord-api-types/v9";
 import { getCommand, getOrLoadAllCommands } from "../handlers/commandHandler";
 import { getBugReportButton, getDiscordInviteButton, getFeatureRequestButton } from "../helpers/messageHelpers";
 import type { NeedleCommand } from "../types/needleCommand";
