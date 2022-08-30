1d0c5d06be34b65e0623ad10f68a3890871b0168
MarcusOtter <github@otterstrom.dev>
2022-08-30 16:56:32 +0200

    Add license notices

--- src/models/enums/ListenerRunType.ts
+++ src/models/enums/ListenerRunType.ts
@@ -1,12 +1,23 @@
+/*
+This file is part of Needle.
+
+Needle is free software: you can redistribute it and/or modify it under the terms of the GNU
+Affero General Public License as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+Needle is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with Needle.
+If not, see <https://www.gnu.org/licenses/>.
+*/
+
 enum ListenerRunType {
-	/**
-	 * Run this listener every time an the event is emitted.
-	 */
+	/** Run this listener every time an the event is emitted. */
 	EveryTime = "on",
 
-	/**
-	 * Only run this listener the first time the event is emitted.
-	 */
+	/** Only run this listener the first time the event is emitted. */
 	OnlyOnce = "once",
 }
 
